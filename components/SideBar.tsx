b67ee2ff173c81343cee866e8e124f1317f97da8
idilnsk <idilcalislar@gmail.com>
2023-11-09 13:59:26 +0100

    chat input

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,44 @@
-import NewChat from"./NewChat"
+"use client";
 
-function SideBar (){
-    return <div className="p-2 flex flex-col h-screen"> 
-    <div className="flex-1">
-        <div>
+import { useSession, signOut } from "next-auth/react";
+import NewChat from "./NewChat";
+import {useCollection} from "react-firebase-hooks/firestore"
+import { db } from "../firebase";
+import {collection, orderBy, query} from "firebase/firestore";
+import ChatRow from "./ChatRow";
+function SideBar() {
+  const { data: session } = useSession();
 
-        {/*newChat*/}
-        <NewChat/>
+  const [chats, loading, error] = useCollection (
+    session && query(collection(db, 'users', session.user?.email!, 'chats'), orderBy("createdAt", "asc")
+  ));
+
+  console.log(chats);
+
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
         <div>
-            {/*ModalSelection */}
+          {/*newChat*/}
+          <NewChat />
+          <div>{/*ModalSelection */}</div>
+
+          {/*Map through the ChatRows*/}
+          {chats?.docs.map(chat =>(
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
+      </div>
 
-        {/*Map through the ChatRows*/}
+      {session && 
+      <img 
+      onClick={()=> signOut()}
+      src={session.user?.image!} alt="Profile picture"
+      className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+      />}
+      
     </div>
-    </div>
-    </div>
-};
+  );
+}
 
 export default SideBar;
